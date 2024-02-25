c358e97af3fe57f3c0e18c6663c76d373e8e2faa
Thunman <Thunman42@gmail.com>
2024-02-25 23:54:51 +0100

    server and ssh connection done

--- main.js
+++ main.js
@@ -1,31 +1,46 @@
+import express from "express";
+import bodyParser from "body-parser";
 import { Client } from "ssh2";
 import dotenv from "dotenv";
 
 dotenv.config();
-let conn = new Client();
-conn
-	.on("ready", function () {
-		console.log("Client :: ready");
-		conn.exec("ls", function (err, stream) {
-			if (err) throw err;
-			stream
-				.on("close", function (code, signal) {
-					console.log(
-						"Stream :: close :: code: " + code + ", signal: " + signal
-					);
-					conn.end();
-				})
-				.on("data", function (data) {
-					console.log("STDOUT: " + data);
-				})
-				.stderr.on("data", function (data) {
-					console.log("STDERR: " + data);
-				});
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post("/execute-command", (req, res) => {
+	const command = req.body.command;
+	let conn = new Client();
+
+	conn
+		.on("ready", function () {
+			console.log("Client :: ready");
+			conn.exec(command, function (err, stream) {
+				if (err) return res.status(500).send(err);
+
+				stream
+					.on("close", function (code, signal) {
+						console.log(
+							"Stream :: close :: code: " + code + ", signal: " + signal
+						);
+						conn.end();
+						res.send("Command executed with code: " + code);
+					})
+					.on("data", function (data) {
+						console.log("STDOUT: " + data);
+					})
+					.stderr.on("data", function (data) {
+						console.log("STDERR: " + data);
+					});
+			});
+		})
+		.connect({
+			host: process.env.HOST_IP,
+			port: 22,
+			username: process.env.USERNAME,
+			password: process.env.PASSWORD,
 		});
-	})
-	.connect({
-		host: process.env.HOST_IP,
-		port: 22,
-		username: process.env.USERNAME,
-		password: process.env.PASSWORD,
-	});
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
